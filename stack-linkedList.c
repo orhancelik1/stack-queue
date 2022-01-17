a5c6ba854b092d68d01e023d0e2030b9a2b256d9
orhan <orhankagan33@gmail.com>
2022-01-17 16:58:53 +0300

    added reset and size function

--- stack-linkedList.c
+++ stack-linkedList.c
@@ -1,18 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct Node{
+typedef struct STACK{
     int data;
-    struct Node* next;
-}Node;
+    struct STACK* next;
+}STACK;
 
-Node* top = NULL;
+STACK* top = NULL;
 
-Node* getNewNode(int);
+STACK* getNewSTACK(int);
 void Push(int);
 void Pop();
 void Print();
 void Front();
+void Reset();
+int Size();
 
 int main(int argc, char const *argv[])
 {
@@ -24,22 +26,24 @@ int main(int argc, char const *argv[])
     Pop();Print();   //1 4 2
     Pop();Print();   //4 2
     Push(7);Print(); //7 4 2
+    printf("size of stack: %d\n",Size()); //3
     Front(); //7
+    Reset();Print();
     
     return 0;
 }
-//getting new node
-Node* getNewNode(int data){
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
+//getting new STACK
+STACK* getNewSTACK(int data){
+    STACK* newSTACK = (STACK*)malloc(sizeof(STACK));
+    newSTACK->data = data;
+    newSTACK->next = NULL;
 
-    return newNode;
+    return newSTACK;
 }
 
 //inserting head (top)
 void Push(int data){
-    Node* temp = getNewNode(data);
+    STACK* temp = getNewSTACK(data);
     if(top == NULL){
         top = temp;
         return;
@@ -55,30 +59,49 @@ void Pop(){
         printf("there is no element in the stack.\n");
         return;
     }
-    Node* temp = top;
+    STACK* temp = top;
     top = temp->next;
     free(temp);
 
 }
 
 void Print(){
-    Node* temp = top;
+    STACK* temp = top;
     if(top == NULL){
         printf("there is no element in the stack.\n");
         return;
     }
-    printf("Stack is: ");
+    
     while(temp!= NULL){
-        printf("%d->",temp->data);
+        printf("\n| %d |",temp->data);
         temp = temp->next;
     }
-    printf("\n");
+    printf("\n------\n");
 }
 
 void Front(){
     if(top == NULL){
-        printf("there is no element.");
+        printf("there is no element.\n");
         return;
     }
-    printf("top element is: %d",top->data);
+    printf("top element is: %d\n",top->data);
+}
+
+void Reset(){
+    STACK *temp;
+    while(top){
+        temp = top;
+        top = top->next;
+        free(temp);
+    }
+}
+
+int Size(){
+    int size = 0;
+    STACK *temp = top;
+    while(temp){
+        temp = temp->next;
+        size++;
+    }
+    return size;
 }
